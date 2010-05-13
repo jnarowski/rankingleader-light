93dc14a8b1c8ce2116d9e0fda655701d39c840a1
John Paul Narowski <jnarowski@gmail.com>
2010-05-13 16:01:53 -0400

    capified the app

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+# required
+set :application, "rankingleader_light"
+set :deploy_to, "/var/www/rails/#{application}"
+set :domain, "metajp.com"
+set :repository,  "#{scm_username}@github.com:jnarowski/rankingleader-light.git"
+
+# extra options
+default_run_options[:pty] = true
+set :use_sudo, false
+set :shared_children, %w(system log pids config)
+
+# defaults
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+namespace :deploy do
+  task :start do; end
+  task :stop do; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+#----------------------------------------------------------------
+# hooks to setup all the fun
+#----------------------------------------------------------------
+
+after "deploy:symlink", "db:mysql:symlink"
+after "deploy:symlink", "wordpress:create_symlinks"
